d39bcab6d23013838e5dd443345d83a42692a7a8
andjyy <bjekovic.andjela@gmail.com>
2023-05-30 02:18:56 +0200

    Baze

--- model.go
+++ model.go
@@ -3,6 +3,7 @@ package main
 type Config struct {
 	Id      string            `json:"id"`
 	Entries map[string]string `json:"entries"`
+	Version string            `json:"version"`
 }
 
 type Group struct {
