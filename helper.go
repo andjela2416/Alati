687ce7e5865eba2ede489f57c98e6bb46c1600ae
andjyy <bjekovic.andjela@gmail.com>
2023-06-05 23:26:51 +0200

    tracer

--- helper.go
+++ helper.go
@@ -1,37 +1,51 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"example.com/mod/store"
+	tracer "example.com/mod/tracer"
 	"github.com/google/uuid"
 	"io"
 	"net/http"
 )
 
-func decodeBody(r io.Reader) (*store.Config, error) {
+func decodeBody(ctx context.Context, r io.Reader) (*store.Config, error) {
+	span := tracer.StartSpanFromContext(ctx, "decodeBody")
+	defer span.Finish()
+
 	dec := json.NewDecoder(r)
 	dec.DisallowUnknownFields()
 	var c store.Config
 	if err := dec.Decode(&c); err != nil {
+		tracer.LogError(span, err)
 		return nil, err
 	}
 	return &c, nil
 }
 
-func decodeGroup(r io.Reader) (*store.Group, error) {
+func decodeGroup(ctx context.Context, r io.Reader) (*store.Group, error) {
+	span := tracer.StartSpanFromContext(ctx, "decodeBody")
+	defer span.Finish()
+
 	dec := json.NewDecoder(r)
 	dec.DisallowUnknownFields()
 
 	var g store.Group
 	if err := dec.Decode(&g); err != nil {
+		tracer.LogError(span, err)
 		return nil, err
 	}
 	return &g, nil
 }
 
-func renderJSON(w http.ResponseWriter, v interface{}) {
+func renderJSON(ctx context.Context, w http.ResponseWriter, v interface{}) {
+	span := tracer.StartSpanFromContext(ctx, "decodeBody")
+	defer span.Finish()
+
 	js, err := json.Marshal(v)
 	if err != nil {
+		tracer.LogError(span, err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
