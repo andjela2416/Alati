bf8baaadf85b74c26894cf679afa243fe6e418a5
andjyy <bjekovic.andjela@gmail.com>
2023-06-14 02:24:15 +0200

    pass

--- test/example_test.go
+++ test/example_test.go
@@ -15,7 +15,7 @@ func TestAlwaysFailing(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Test failed. Expected: %d, but got: %d", expected, actual)
 	}
-} */
+}  */
 
 func TestPassed(t *testing.T) {
 	// Perform some test setup
